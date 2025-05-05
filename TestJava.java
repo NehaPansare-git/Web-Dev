33bd3a288886c6d533aefe50d4e7744ddc7f5567
NehaPansare-git <2021wa86731@wilp.bits-pilani.ac.in>
2025-05-05 19:05:55 +0530

    Updated Java test script - Version 2

--- TestJava.java
+++ TestJava.java
@@ -1,2 +1,3 @@
 echo “public class Testjava {public static void main(String[] args) { System.out.println(\”Java Test\”); } }" > TestJava.java
   echo “public class Testjava {public static void main(String[] args) { System.out.println(\”version 1\”); } }" > TestJava.java 
+  // Version 2 - Added logging feature
