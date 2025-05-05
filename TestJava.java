8861d607f531f291d55aa7125c278ff57dace208
NehaPansare-git <2021wa86731@wilp.bits-pilani.ac.in>
2025-05-05 19:05:16 +0530

    Added Java test script - Version 1

--- TestJava.java
+++ TestJava.java
@@ -1,1 +1,2 @@
-echo “public class Testjava {public static void main(String[] args) { System.out.println(\”Java Test\”); } }" > TestJava.java 
+echo “public class Testjava {public static void main(String[] args) { System.out.println(\”Java Test\”); } }" > TestJava.java
+  echo “public class Testjava {public static void main(String[] args) { System.out.println(\”version 1\”); } }" > TestJava.java 
